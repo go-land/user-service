b9b68192a8846823d70091d76193c611062441fc
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-12-10 15:09:15 +0200

    Consul added as service registry instead of mDNS.

--- cli/run.sh
+++ cli/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
+sleep 15;
+
 echo "********************************************************"
 echo "Running user-cli"
 echo "********************************************************"
-
-while ! `nc -z user-service1 8080`; do sleep 3; done
-while ! `nc -z user-service2 8080`; do sleep 3; done
 
 ./user-cli
